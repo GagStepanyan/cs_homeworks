802645c2db0f2cf510fcf608b25b2e643ca91f12
GagStepanyan <gag.stepanyan9@gmail.com>
2025-03-02 02:24:40 +0400

    7th Docs

---
+++ 7TASKS/DigitalInkReservoir/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+
+Digital Ink Reservoir
+   Concept: Simulate an ink reservoir in a digital pen where ink can be added or removed when writing.
+   Requirements
+Implement a InkReservoir class:
+1. Color (string)
+2. InkAmount (double, in milliliters)
+3. Overload + operator:
+    Combine two ink reservoirs (if they have the same color).
+    If colors differ, prevent merging.
+4. Overload - operator:
+    Subtract ink from a reservoir (simulating ink usage).
+    Prevent negative ink levels.
+5. Override ToString() to return reservoir details.
+
+*/
+
+using System;
+
+class InkReservoir {
+    string? _inkColor;
+    double _inkAmount;
+
+    public InkReservoir(string color, double amount) {
+        if (amount < 0 || string.IsNullOrEmpty(color)) {
+            throw new ArgumentException("INVALID ARGUMENT");
+        }
+
+        _inkColor = color;
+        _inkAmount = amount;
+    }
+
+    public static InkReservoir operator +(InkReservoir reservoir1, InkReservoir reservoir2) => AddReservoirs(reservoir1, reservoir2);
+    public static InkReservoir AddReservoirs(InkReservoir reservoir1, InkReservoir reservoir2) {
+        if (reservoir1._inkColor != reservoir2._inkColor) {
+           throw new ArgumentException("Colors Dont Match");
+        }
+
+        return new InkReservoir(reservoir1._inkColor!, reservoir1._inkAmount + reservoir2._inkAmount);
+    }
+
+    //Simulating spending the ink on string writing
+    public static InkReservoir operator -(InkReservoir reservoir, string str) => SubReservoirs(reservoir, str);
+    public static InkReservoir SubReservoirs(InkReservoir reservoir, string str) {
+        if (str.Length > reservoir._inkAmount) {
+           throw new ArgumentException("the string is too long");
+        }
+
+        return new InkReservoir(reservoir._inkColor!, reservoir._inkAmount - str.Length);
+    }
+
+    public override string ToString() => $"Ink Color:{_inkColor}\nInk Amount: {_inkAmount}\n";
+
+    public void Display() {
+        Console.WriteLine(this.ToString());
+    }
+}
+
+class Program {
+    public static void Main(string[] args) {
+        InkReservoir inkReservoir1 = new InkReservoir("black", 10);
+        InkReservoir inkReservoir2 = new InkReservoir("red", 100);
+        InkReservoir inkReservoir3 = new InkReservoir("black", 350);
+
+        InkReservoir res = inkReservoir1 + inkReservoir3;
+        res.Display();
+
+        //using 26 ml's of ink
+        inkReservoir3 -= "this string has 26 symbols";
+        inkReservoir3.Display();
+
+        //res = inkReservoir3 + inkReservoir2; //will cause an exception;
+    }
+}
---
+++ 7TASKS/WaterTankSystem/Program.cs
@@ -0,0 +1,90 @@
+﻿/*
+
+Task 1: Water Tank System
+Concept: Simulate water tanks where water can be transferred or consumed.
+Requirements
+Implement a WaterTank class:
+1. Capacity (double, liters)
+2. CurrentLevel (double, liters)
+3. Overload + operator: Combine two water tanks (capped at the capacity limit).
+4. Overload - operator: Consume water from the tank (cannot go below 0).
+5. Override ToString() to display tank levels.
+
+*/
+
+using System;
+
+class WaterTank {
+    double _tankCapacity;
+    double _tankCurrentLevel;
+
+    public WaterTank(double currentLevel = 0) {
+        _tankCapacity = 1000;
+        if (currentLevel < 0 || currentLevel > _tankCapacity) {
+            throw new ArgumentException("invalid argument");
+        }
+        _tankCurrentLevel = currentLevel;
+
+    }
+
+    //allowing user to add water directly
+    public void AddWater(double liters) {
+        if (_tankCurrentLevel + liters > _tankCapacity) {
+            Console.WriteLine("there is no enough space");
+            return;
+        } else {
+            _tankCurrentLevel += liters;
+        }
+    }
+
+
+
+    public static WaterTank operator +(WaterTank tank1, WaterTank tank2) => new WaterTank(AddCurrents(tank1, tank2));
+    public static double AddCurrents(WaterTank tank1, WaterTank tank2) {
+        if (tank1._tankCurrentLevel + tank2._tankCurrentLevel > tank1._tankCapacity) {
+            Console.WriteLine("not enaugh capacity");
+            return -1;
+        }
+        return tank1._tankCurrentLevel + tank2._tankCurrentLevel;
+    }
+
+    public static WaterTank operator -(WaterTank tank1, double liters) => new WaterTank(SubCurrents(tank1, liters));
+    public static double SubCurrents(WaterTank tank1, double liters) {
+        if (tank1._tankCurrentLevel - liters < 0) {
+            Console.WriteLine("goin below zero");
+            return -1;
+        }
+
+        return (tank1._tankCurrentLevel - liters);
+    }
+
+
+    public override string ToString() {
+        return $"{_tankCurrentLevel}/{_tankCapacity}";
+    }
+
+    public void Display() {
+        Console.WriteLine(this.ToString());
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        WaterTank tank1 = new WaterTank(900);
+        WaterTank tank2 = new WaterTank(250);
+        
+        WaterTank res = tank1 - 100;
+        res.Display();
+        
+        res = tank1 - tank2;
+        res.Display();
+
+        Cosnole.Write("Water Tank1: ");
+        tank1.Display();
+        
+        Cosnole.Write("Water Tank2: ");
+        tank2.Display();
+
+
+    }
+}
