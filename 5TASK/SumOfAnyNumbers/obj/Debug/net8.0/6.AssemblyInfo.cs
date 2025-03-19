48dd0f622403f8784d9ce7e11d9a7ffcf2fe658e
GagStepanyan <gag.stepanyan9@gmail.com>
2025-03-19 18:43:11 +0400

    updated

--- 5TASK/SumOfAnyNumbers/obj/Debug/net8.0/6.AssemblyInfo.cs
+++ 5TASK/SumOfAnyNumbers/obj/Debug/net8.0/6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bea4fc79f7431400fed013dd7e36ab45495e156")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438357d8482d002697f80b701e6170c06cc1e225")]
 [assembly: System.Reflection.AssemblyProductAttribute("6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
