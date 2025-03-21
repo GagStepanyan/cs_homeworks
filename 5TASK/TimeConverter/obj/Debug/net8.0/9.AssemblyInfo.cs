773516a3d3e6810269a846e965d960b36fb20d38
GagStepanyan <gag.stepanyan9@gmail.com>
2025-03-22 01:55:20 +0400

    modified

--- 5TASK/TimeConverter/obj/Debug/net8.0/9.AssemblyInfo.cs
+++ 5TASK/TimeConverter/obj/Debug/net8.0/9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438357d8482d002697f80b701e6170c06cc1e225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48dd0f622403f8784d9ce7e11d9a7ffcf2fe658e")]
 [assembly: System.Reflection.AssemblyProductAttribute("9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
